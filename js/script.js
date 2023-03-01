111832ab9838fdbd3645e41552c174a0bcc66db3
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-02 00:20:10 +0100

    input for insert one by one the element

--- js/script.js
+++ js/script.js
@@ -24,8 +24,13 @@ console.log(listaSpesa);
 let listEl = document.getElementById("list");
 
 let buttonListGeneratorEl = document.getElementById("buttonListGenerator");
+let buttonListDeleteEl = document.getElementById("buttonListDelete");
 
+
+// pulsante per generare la lista data
 buttonListGeneratorEl.addEventListener("click", function() {
+
+    listEl.style.display = "flex";
 
     while (index < listaSpesa.length) {
         // creo tanti elementi li quanti sono gli elementi della spesa
@@ -38,12 +43,80 @@ buttonListGeneratorEl.addEventListener("click", function() {
         newLiElement.classList.add("list-item");
     
         // scrivo il contenuto, all'interno del DOM
-        newLiElement.innerHTML = (listaSpesa[index] + "<br>")
+        newLiElement.innerHTML = ((index + 1) + ". " + listaSpesa[index] + "<br>")
     
     
         // per ogni volta che fa il controllo, index dovrà aumentare di 1: 0-1-2-3-4-5-6...
         index++;
+
     
     }
 
 })
+
+
+// pulsante per eliminare la lista
+buttonListDeleteEl.addEventListener("click", function() {
+
+    listEl.style.display = "none";
+
+})
+
+
+
+
+
+
+
+// // metodo opzionale con il "inserisci elemento" cliccando sul bottone, non avendo una lista data 
+
+
+// // creo lista della spesa
+// let listaSpesa = [];
+// // creo variabile sentinella inizializzate = 0
+// let index = 0;
+
+// let inputItemEl = document.getElementById("inputItem");
+
+// console.log(listaSpesa);
+
+// // per stilizzare la mia listaSpesa inizio con l'inizializzare il contenitore di questi
+// let listEl = document.getElementById("list");
+
+// let buttonListGeneratorEl = document.getElementById("buttonListGenerator");
+// let buttonListDeleteEl = document.getElementById("buttonListDelete");
+
+// buttonListGeneratorEl.addEventListener("click", function() {
+    
+//     listEl.style.display = "flex";
+
+//     listaSpesa.push(inputItemEl.value);
+
+//     while (index < listaSpesa.length) {
+//         // creo tanti elementi li quanti sono gli elementi della spesa
+//         let newLiElement = document.createElement("li");
+        
+//         // faccio riferimento al genitore
+//         listEl.append(newLiElement);
+        
+//         // aggiungo a tutti gli elementi create la classe list-item stilizzata in CSS
+//         newLiElement.classList.add("list-item");
+    
+
+//            // scrivo il contenuto, all'interno del DOM
+//          newLiElement.innerHTML = ((index + 1) + ". " + listaSpesa[index] + "<br>")
+    
+//         index++
+//         // per ogni volta che fa il controllo, index dovrà aumentare di 1: 0-1-2-3-4-5-6...
+        
+    
+//     }
+
+// })
+
+// // pulsante per eliminare la lista
+// buttonListDeleteEl.addEventListener("click", function() {
+
+//     listEl.style.display = "none";
+
+// })
