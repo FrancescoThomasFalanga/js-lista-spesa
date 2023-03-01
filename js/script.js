e255753e75605f430150e318daf02ee767ba956a
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-01 16:40:20 +0100

    add listaSpesa

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ Data una lista della spesa (memorizzata in un array),
 stampare sulla pagina (anche brutalmente, basta che si vedano) 
 gli elementi della lista individualmente con un ciclo while.
 */
+
+/*
+- creare una lista della spesa
+- stampare la lista della spesa in console
+- stampare individualmente gli elementi in pagina
+*/
+
+
+
+
+// creo lista della spesa
+let listaSpesa = ["Pane", "Arance", "Latte", "Lattuga", "Pomodori", "Prosciutto", "Bibite", "Detergente", "Spaghetti", "Pesce", "Formaggio", "Ceci", "Piselli", "Uova", "Succo", "Caffè", "Tonno", "Cereali", "Biscotti" ,"Piatti di Carta"];
+// creo variabile sentinella inizializzate = 0
+let index = 0;
+
+console.log(listaSpesa);
+
+
+while (index < listaSpesa.length) {
+
+    document.writeln(listaSpesa[index] + "<br>");
+    index++
+
+}
